0e742f76d3ee0dcc849cfe4fc470bceb60026aaf
shivkumarpatil <shivva45@gmail.com>
2024-09-19 23:32:41 +0530

    Update build-and-push.sh

--- build-and-push.sh
+++ build-and-push.sh
@@ -6,7 +6,7 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 # Build and push Docker image
 #docker buildx build --platform linux/amd64,linux/arm64 -t user/repo:latest --push .
 # Build the Docker image
-docker build -t user/repo:latest .
+docker build -t shivkumar/github-action:latest .
 
 # Push the Docker image
-docker push user/repo:latest
+docker push shivkumar/github-action:latest
