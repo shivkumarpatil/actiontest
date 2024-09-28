9af63b181b2ab2ebe61ac63e6a4b1014549b7045
shivkumarpatil <shivva45@gmail.com>
2024-09-29 03:39:45 +0530

    Update build-and-push.sh

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Log in to Docker Hub
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+#echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 # Build and push Docker image
 #docker buildx build --platform linux/amd64,linux/arm64 -t user/repo:latest --push .
@@ -11,9 +11,28 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 #docker push shivkumar/github-action:latest
 
 # Build the Docker image
-docker build -t shivkumar/github-action:${GITHUB_REF_NAME} .
-
-
+#docker build -t shivkumar/github-action:${GITHUB_REF_NAME} .
 
 # Push the Docker image with the specific tag
-docker push shivkumar/github-action:${GITHUB_REF_NAME}
+#docker push shivkumar/github-action:${GITHUB_REF_NAME}
+
+#!/bin/bash
+
+# Log in to Docker Hub
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+# Extract the version tag from the GitHub reference
+VERSION_TAG=${GITHUB_REF_NAME#refs/tags/}
+
+# Build the Docker image with the specific version tag
+docker build -t shivkumar/github-action:${VERSION_TAG} .
+
+# Push the Docker image with the specific version tag
+docker push shivkumar/github-action:${VERSION_TAG}
+
+# Tag the image as latest
+docker tag shivkumar/github-action:${VERSION_TAG} shivkumar/github-action:latest
+
+# Push the latest tag
+docker push shivkumar/github-action:latest
+
