5c1516edaae32b1cc77cfe51668f7e9fb3725f4e
shivkumarpatil <shivva45@gmail.com>
2024-09-29 03:35:13 +0530

    Update build-and-push.sh

--- build-and-push.sh
+++ build-and-push.sh
@@ -6,7 +6,14 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 # Build and push Docker image
 #docker buildx build --platform linux/amd64,linux/arm64 -t user/repo:latest --push .
 # Build the Docker image
-docker build -t shivkumar/github-action:latest .
-
+#docker build -t shivkumar/github-action:latest .
 # Push the Docker image
-docker push shivkumar/github-action:latest
+#docker push shivkumar/github-action:latest
+
+# Build the Docker image
+docker build -t shivkumar/github-action:${GITHUB_REF_NAME} .
+
+
+
+# Push the Docker image with the specific tag
+docker push shivkumar/github-action:${GITHUB_REF_NAME}
